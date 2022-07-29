8e16d3cabf334bcdacc2d830ae113e9a0d8fed32
aaansari123 <asadali1029@gmail.com>
2022-07-28 20:38:51 -0400

    finished up script functionality. Added comments and a README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Mod-05-Scheduler-Ansari
+
+This contains the Module 5 Challenge
+## Description
+- This project was to create an event scheduler
+- The goal was to create a color coded scheduler that would tell us what times are in the past present and future.
+- Furthermore it would allow us to save our inputs, so on refresh they would still apear
+- Working on this project showed the importance of keeping my code organized. Creating all of the functions, planning out what elements of the DOC would be used etc.
+## Usage
+
+- to use this site simply click on the field where you wish to make and appointment
+- enter the appointment details and then press save
+- the saved values should show up again on refresh
+
+## Screenshot
+![screenshot of the page](./assets/screenshot.png)
+
+## Link
+
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
-// console.log(x.getAttribute("id"));
 
-// function to figure out past present future
+
+// get all imporatant elements
+var headerElement = document.querySelector(".lead");
 var eightAm = document.getElementById("8");
 var nineAm = document.getElementById("9");
 var tenAm = document.getElementById("10");
@@ -11,14 +12,35 @@ var twoPm = document.getElementById("14");
 var threePm = document.getElementById("15");
 var fourPm = document.getElementById("16");
 var fivePm = document.getElementById("17");
+var saveButton = document.querySelector(".saveBtn1");
+var saveButton2 = document.querySelector(".saveBtn2");
+var saveButton3 = document.querySelector(".saveBtn3");
+var saveButton4 = document.querySelector(".saveBtn4");
+var saveButton5 = document.querySelector(".saveBtn5");
+var saveButton6 = document.querySelector(".saveBtn6");
+var saveButton7 = document.querySelector(".saveBtn7");
+var saveButton8 = document.querySelector(".saveBtn8");
+var saveButton9 = document.querySelector(".saveBtn9");
+var saveButton10 = document.querySelector(".saveBtn10");
+var saveButtons = [saveButton, saveButton2, saveButton3, saveButton4, saveButton5,saveButton6,saveButton7,saveButton8,saveButton9, saveButton10];
+var appointment1 = document.querySelector(".description1");
+var appointment2 = document.querySelector(".description2");
+var appointment3 = document.querySelector(".description3")
+var appointment4 = document.querySelector(".description4")
+var appointment5 = document.querySelector(".description5")
+var appointment6 = document.querySelector(".description6")
+var appointment7 = document.querySelector(".description7")
+var appointment8 = document.querySelector(".description8")
+var appointment9 = document.querySelector(".description9")
+var appointment10 = document.querySelector(".description10");
+var appointments = [appointment1, appointment2, appointment3, appointment4, appointment4, appointment5, appointment6, appointment7, appointment8, appointment9, appointment10];
 
+// function to color code
 function pastPresentFuture(){
     var now = moment().hour();
     for (var i = 8; i < 18; i++){
         var timeIs = document.getElementById(i);
         var time = timeIs.getAttribute("id");
-        console.log(timeIs);
-        console.log(time);
         if (time < now){
             timeIs.classList.toggle("past");
         } else if(time == now){
@@ -33,7 +55,47 @@ function pastPresentFuture(){
 
 }
 
-// var now = moment().hour();
-// console.log(now);
 
+// function for handling save button click
+
+function handleSave(event){
+    var inputVal;
+    event.preventDefault();
+    for (i = 0; i < appointments.length; i++){
+        inputVal = $(appointments[i]).val();
+        localStorage.setItem(i, inputVal);
+    }
+    
+
+
+    // $(appointment).val(inputVal);
+    console.log("test");
+}
+
+function addEventListeners(){
+    for (var i = 0; i< saveButtons.length; i++){
+        saveButtons[i].addEventListener('click',handleSave);
+    }
+}
+
+// repopulates with saved values
+function refillValues(){
+    var x;
+    for (var i = 0; i < appointments.length; i++){
+        x = localStorage.getItem(i);
+        $(appointments[i]).val(x);
+    }
+    
+
+}
+// update time at top of page
+function updateTime(){
+    var currentTime = moment().format("LLLL" );
+    headerElement.innerHTML += "<br>" + currentTime;
+}
+
+// call all functions
 pastPresentFuture();
+refillValues();
+addEventListeners();
+updateTime();
